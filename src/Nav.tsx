617c9cda1155c5a87b87f7d799c50648a8390ee1
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-31 10:03:50 -0500

    init commit

---
+++ src/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <>
+      <nav className='header__nav'>
+        <ul className='header__items'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+          <li>Profile</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
